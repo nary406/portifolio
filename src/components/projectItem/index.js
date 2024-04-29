3381f2d15835398090467d2ba0e59fdd93a7aaf4
nary406 <naryxyz3@gmail.com>
2024-04-29 11:24:31 +0530

    h

--- src/components/projectItem/index.js
+++ src/components/projectItem/index.js
@@ -2,40 +2,42 @@ import React from 'react'
 import gsap from "gsap"
 import { useRef, useEffect, useState } from 'react';
 import "./index.css"
-import {ScrollTrigger} from "gsap/ScrollTrigger"
+import ScrollTrigger from "gsap/ScrollTrigger"
 gsap.registerPlugin(ScrollTrigger)
 
 function ProjectItem(props) {
     const {name, id, description, image}=props.item
     
 const {classy}=props
+
     const but=useRef(null)
     const contain=useRef(null)
 
 
-    useEffect(()=>{
-
-      gsap.fromTo(but.current, {  transform:"scale(0)"}, {transform:"scale(1)",   ease: "slow(0.7,0.7,false)", scrollTrigger:{
-        start:"top 90%",
-        end:"top 70%",
-        scrub:3,
-        trigger:classy,
-      
-      }});
+    useEffect(() => {
+      gsap.fromTo(
+        but.current,
+        { transform: "scale(0)" }, // initial state
+        { transform: "scale(1)", duration: 1, ease: "slow(0.7,0.7,false)", scrollTrigger: {
+            start: "top 90%",
+            end: "top 60%",
+            scrub: 3,
+            trigger: classy.current,
+          } 
+        } // animate to scale 1
+      );
+    }, [props]);
     
-
-
-},[])
    
    
   return (
-    <div className="container"  ref={but}>
-        <div className="p_box"  onMouseEnter={()=>console.log("card")}  >
-        <div className="flip_card" onClick={()=>console.log("flip")}>
+    <div className="container" ref={but}  >
+        <div className="p_box"  >
+        <div className="flip_card" >
           <p style={{fontSize:"15px"}}>{description}</p>
             <button>View Project</button>
         </div>
-        <img src={image} className="p_img"/>
+        <img src={image} className="p_img"  ref={but}/>
       <p>{name}</p>
      
     </div>
